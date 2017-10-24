f99f39c1374857ae8feceb48e59d53a062f3f305
Hien Nguyen <quochien@gmail.com>
2017-10-24 21:01:43 +0700

    create genre

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -31,12 +31,40 @@ exports.genre_detail = function(req, res) {
 
 // Display Genre create form on GET
 exports.genre_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre create GET');
+  res.render('genre_form', { title: 'Create Genre' });
 };
 
 // Handle Genre create on POST
-exports.genre_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre create POST');
+exports.genre_create_post = function(req, res, next) {
+  req.checkBody('name', 'Genre name required').notEmpty();
+
+  req.sanitize('name').escape();
+  req.sanitize('name').trim();
+
+  var errors = req.validationErrors();
+
+  var genre = new Genre({ name: req.body.name });
+
+  if (errors) {
+    res.render('genre_form', { title: 'Create Genre', genre: genre, errors: errors });
+    return;
+  }
+  else {
+    Genre.findOne({ 'name': req.body.name })
+      .exec(function(err, found_genre) {
+        if (err) { return next(err); }
+
+        if (found_genre) {
+          res.redirect(found_genre.url);
+        }
+        else {
+          genre.save(function(err) {
+            if (err) { return next(err); }
+            res.redirect(genre.url);
+          });
+        }
+      });
+  }
 };
 
 // Display Genre delete form on GET
